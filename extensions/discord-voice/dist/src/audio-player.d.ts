c00061034381da6c7d3f495013589ab072dbb5e8
Clause <clause2k@gmail.com>
2026-01-29 07:26:56 +0000

    Ignore build output and node_modules for discord-voice

--- extensions/discord-voice/dist/src/audio-player.d.ts
+++
@@ -1,9 +0,0 @@
-import { VoiceConnection } from "@discordjs/voice";
-export declare class VoicePlayer {
-    private readonly ffmpegPath;
-    private player;
-    constructor(ffmpegPath: string);
-    play(pcmBuffer: Buffer, connection: VoiceConnection): Promise<void>;
-    private resampleToDiscord;
-}
-//# sourceMappingURL=audio-player.d.ts.map
--- extensions/discord-voice/dist/src/tts-service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class PiperTTS {
-    private readonly piperPath;
-    private readonly modelPath;
-    constructor(piperPath: string, modelPath: string);
-    synthesize(text: string): Promise<Buffer>;
-}
-//# sourceMappingURL=tts-service.d.ts.map
